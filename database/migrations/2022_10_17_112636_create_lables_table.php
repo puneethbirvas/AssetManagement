d78d0b94bec47ba70d8ccc5400fd39d5d51aafe5
vaishaksuvarna <vaishakkpoojary@gmail.com>
2022-10-17 17:18:31 +0530

    Lable added

---
+++ database/migrations/2022_10_17_112636_create_lables_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('labels', function (Blueprint $table) {
+            $table->id();
+            $table->string('Department');
+            $table->string('selectSection');
+            $table->string('assetType');
+            $table->string('selectAssetType');
+            $table->string('selectAsset')->nullable();
+            $table->string('selectAssetId')->nullable();
+            $table->string('code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('labels');
+    }
+};
