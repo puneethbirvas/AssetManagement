a753f59d85a46f416e7a936f5fd279351732a4fd
vaishaksuvarna <vaishakkpoojary@gmail.com>
2022-11-15 14:53:21 +0530

    code and controller added

--- database/migrations/2022_10_17_042642_create_assets_table.php
+++ database/migrations/2022_10_17_042642_create_assets_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
             $table->bigInteger('vendorName')->unsigned();
             $table->foreign('vendorName')->references('id')->on('vendors')->onDelete('cascade');
             $table->string('phoneNumber');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('vendorAddress');
             $table->bigInteger('assetType')->unsigned();
             $table->foreign('assetType')->references('id')->on('assettypes')->onDelete('cascade');
---
+++ database/migrations/2022_11_15_091821_create_maintenances_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('maintenances', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('assetName')->unsigned();
+            $table->foreign('assetName')->references('id')->on('assets')->onDelete('cascade');
+            $table->string('maintenanceId');
+            $table->string('maintenanceType');
+            $table->string('severity');
+            $table->string('problemNote');
+            $table->string('bpImages1');
+            $table->string('bpImages2')->nullable();;
+            $table->string('bpImages3')->nullable();;
+            $table->string('bpImages4')->nullable();;
+            $table->string('partsOrConsumable');
+            $table->string('affectedMachine');
+            $table->string('affectedManHours');
+            $table->string('shutdownOrUtilization')->nullable();
+            $table->string('machineDetails')->nullable();
+            $table->string('offOrUtilization')->nullable();
+            $table->string('manHoursDetails')->nullable();
+            $table->string('dateFrom');
+            $table->string('dateTo');
+            $table->string('timeFrom');
+            $table->string('timeTo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('maintenances');
+    }
+};
